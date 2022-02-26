13682409284487a802746d5709689d0932a8d830
[aslan-asilon3] <[sulaslansetiawan3@gmail.com]>
2022-02-26 15:47:05 +0800

    add datatype in php

---
+++ basic/datatype.php
@@ -0,0 +1,14 @@
+<?php
+
+$a = "ini adalah string"; //string
+$b = 18; //integger
+$c = 12.5; //float
+$d = false; //boolean
+$e = [
+    'satu',
+    'dua',
+    'tiga',
+]; // array
+$f = NULL; //null
+
+# fungsi var_dump untuk mengecek tipe data
