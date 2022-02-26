fe30014b6a35b9fa01d8009808533dfed50edd78
[aslan-asilon3] <[sulaslansetiawan3@gmail.com]>
2022-02-26 16:10:09 +0800

    add operator in php

---
+++ basic/operator.php
@@ -0,0 +1,27 @@
+<?php
+
+// Operator Aritmatika
+// +, *, - , / , %, **
+
+// operator assignment
+// $a = 8;
+// $b = 10;
+// $total = 10;
+// $total += $a;
+// echo $total; // output: 18
+
+// operator pembanding / comparison
+// $a = 4;
+// $b = 7;
+// $komparasi = $a == $b;
+// echo var_dump($komparasi); //output: bool(false)
+
+// operator increment/decrement
+// $a = 10;
+// $a++;
+// echo $a; //output:11
+
+// operator string
+// $a = "hello";
+// $b = "world";
+// echo $a . $b; //hello aslan
